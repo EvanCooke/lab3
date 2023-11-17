7b9a18352adb3343bf3ee207faf3bb5f8031a675
EmSheshan <71909838+EmSheshan@users.noreply.github.com>
2023-11-17 13:41:01 -0600

    Update firebase-init.js

--- firebase-init.js
+++ firebase-init.js
@@ -2,10 +2,15 @@
 import { initializeApp } from 'https://www.gstatic.com/firebasejs/9.6.8/firebase-app.js';
 import { getAuth, setPersistence, browserLocalPersistence } from 'https://www.gstatic.com/firebasejs/9.6.8/firebase-auth.js';
 
-const firebaseConfig = {
-      apiKey: "AIzaSyDrvtpuL-GJzqpM6dThTqFFbC6BDxkQE-M",
-      authDomain: "lab-3-d8c6a.firebaseapp.com",
-};
+        const firebaseConfig = {
+            apiKey: "AIzaSyDrvtpuL-GJzqpM6dThTqFFbC6BDxkQE-M",
+            authDomain: "lab-3-d8c6a.firebaseapp.com",
+            projectId: "lab-3-d8c6a",
+            storageBucket: "lab-3-d8c6a.appspot.com",
+            messagingSenderId: "910114646771",
+            appId: "1:910114646771:web:3a41ebdeab13bf4a5c2586",
+            measurementId: "G-MQF0BJ881D"
+        };
 
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
